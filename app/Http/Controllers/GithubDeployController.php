9a64859123eb54e6f8aedb5acd6736e57e0edc09
Cris281172 <krzysiu.juczynski@gmail.com>
2025-06-24 16:38:25 +0200

    Update

--- app/Http/Controllers/GithubDeployController.php
+++ app/Http/Controllers/GithubDeployController.php
@@ -12,6 +12,8 @@ class GithubDeployController extends Controller
 
         \Log::info("Webhook started");
 
+        exec("git config --global --add safe.directory /var/www/html");
+
         exec("cd $project_path && git pull origin main 2>&1", $output);
         \Log::info("Git output: " . implode("\n", $output));
 
